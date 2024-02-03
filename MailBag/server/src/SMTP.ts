8f8dcf484e4b968350f38fddca1e0414b33508aa
Vilde Falck Husum <vildefalckhusum@Vildes-Air.lan>
2024-02-03 14:02:29 +0100

    initial

---
+++ MailBag/server/src/SMTP.ts
@@ -0,0 +1,31 @@
+import Mail from "nodemailer/lib/mailer";
+import * as nodemailer from "nodemailer";
+import { SendMailOptions, SentMessageInfo } from "nodemailer";
+//const nodemailer = require("nodemailer");
+import { IServerInfo } from "./serverInfo";
+
+export class Worker {
+    private static serverInfo: IServerInfo;
+    constructor(inServerInfo: IServerInfo) {
+        Worker.serverInfo = inServerInfo;
+    }
+
+    public sendMessage(inOptions: SendMailOptions):
+        Promise<string> {
+        return new Promise((inResolve, inReject) => {
+            const transport: Mail = nodemailer.createTransport(Worker.serverInfo.smtp);
+            transport.sendMail(inOptions,
+                (inError: Error | null, inInfo: SentMessageInfo) => {
+                    if (inError) {
+                        inReject(inError);
+                    } else {
+                        inResolve("");
+                    }
+                }
+            );
+        });
+    }
+}
+
+
+
---
+++ MailBag/server/src/main.ts
@@ -0,0 +1,142 @@
+import path from "path";
+import express,
+{ Express, NextFunction, Request, Response } from "express";
+import { serverInfo } from "./serverInfo";
+import * as IMAP from "./IMAP";
+import * as SMTP from "./SMTP";
+import * as Contacts from "./contacts";
+import { IContact } from "./contacts";
+
+const app: Express = express();
+app.use(express.json());
+app.use("/",
+    express.static(path.join(__dirname, "../../client/dist"))
+);
+
+
+app.listen(80, () => {
+    console.log("App listening on port 3000");
+})
+
+app.use(function (inRequest: Request, inResponse: Response,
+    inNext: NextFunction) {
+    inResponse.header("Access-Control-Allow-Origin", "*");
+    inResponse.header("Access-Control-Allow-Methods",
+        "GET,POST,DELETE,OPTIONS"
+    );
+    inResponse.header("Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept"
+    );
+    inNext();
+});
+
+app.get("/mailboxes",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            console.log("imapworker: ", imapWorker);
+
+            const mailboxes: IMAP.IMailbox[] = await imapWorker.listMailboxes();
+            
+            inResponse.json(mailboxes);
+        } catch (inError) {
+            console.error("Error in mailbox endpoint:", inError);
+            inResponse.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+);
+
+app.get("/mailboxes/:mailbox",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            const messages: IMAP.IMessage[] = await imapWorker.listMessages({
+                mailbox: inRequest.params.mailbox
+            });
+            inResponse.json(messages);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.get("/messages/:mailbox/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            const messageBody: string | undefined = await imapWorker.
+                getMessageBody({
+                    mailbox: inRequest.params.mailbox,
+                    id: parseInt(inRequest.params.id, 10)
+                });
+            inResponse.send(messageBody);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.delete("/messages/:mailbox/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            await imapWorker.deleteMessage({
+                mailbox: inRequest.params.mailbox,
+                id: parseInt(inRequest.params.id, 10)
+            });
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.post("/messages",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const smtpWorker: SMTP.Worker = new SMTP.Worker(serverInfo);
+            await smtpWorker.sendMessage(inRequest.body);
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.get("/contacts",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contacts: IContact[] = await contactsWorker.listContacts();
+            inResponse.json(contacts);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+
+app.post("/contacts",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contact: IContact = await contactsWorker.
+                addContact(inRequest.body);
+            inResponse.json(contact);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.delete("/contacts/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            await contactsWorker.deleteContact(inRequest.params.id);
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
